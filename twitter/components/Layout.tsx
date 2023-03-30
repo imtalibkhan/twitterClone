3199e21837b0e7455cb51756a3e5efb4807ad306
Abutalib Khan <thetalibkhan555@gmail.com>
2023-03-30 13:57:10 +0530

    hello world!!!

---
+++ twitter/components/Layout.tsx
@@ -0,0 +1,14 @@
+interface LayoutProps {
+     children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+
+    )
+}
+
+export default Layout
--- twitter/pages/index.tsx
+++ twitter/pages/index.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
-  return <div className="text-3xl text-sky-500">
-             Hello world
-          </div>;
+  return(
+    <div className="text-3xl text-sky-500">
+      Hello world
+    </div>
+
+  )
+
 }
+  
+  
